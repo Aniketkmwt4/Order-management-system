a5185aae5bdb07f9157e7cac30180fa03ddc6dee
Aniketkmwt4 <aniketkmwt414@gmail.com>
2026-07-08 10:45:24 +0530

    Fixed CORS for production

--- src/main/java/com/aniket/order_management/CorsConfig.java
+++ src/main/java/com/aniket/order_management/CorsConfig.java
@@ -1,3 +1,23 @@
+//package com.aniket.order_management;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(
+//                        "http://localhost:5173",
+//                        "https://order-management-frontend-ten-woad.vercel.app"
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
+//}
 package com.aniket.order_management;
 
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +30,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://order-management-frontend-ten-woad.vercel.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
